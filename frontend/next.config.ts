7c2bbf14197621ef46f7b02fe12c817bc0730742
たっちゃん <138844357+takuhara-alt@users.noreply.github.com>
2025-11-25 19:56:42 +0900

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,14 +1,8 @@
 
-import type { NextConfig } from 'next';
+import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  // Dockerビルドで最小限の実行環境を構築するために必須の設定です
- // output: 'standalone',
-
-  // その他の設定をここに追加します
-  // 例:
-  // reactStrictMode: true,
-  // basePath: '/soc-agent',
+  
 };
 
 export default nextConfig;
